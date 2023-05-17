37111b0f4b41516e76398763ea4aeac457c0f884
eduardo-monita <eduardo.monita.dias@gmail.com>
2023-05-17 03:27:59 -0300

    change permissions

--- huai/shoes/views.py
+++ huai/shoes/views.py
@@ -5,11 +5,13 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 from .models import *
 from .serializers import *
 
 # Create your views here.
 class SimpleWebhook(APIView):
+    permission_classes = [AllowAny]
     
     def post(self, request, *args, **kwargs):
         pass
